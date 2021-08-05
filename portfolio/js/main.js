386763e002c4d1488f3e386445374cdb0a2fde9b
Anand Chauhan <71122607+Anandvlog@users.noreply.github.com>
2021-08-05 15:18:47 +0530

    Add files via upload

---
+++ portfolio/js/main.js
@@ -0,0 +1,17 @@
+// type_text
+var typed = new Typed('.animate', {
+  // Waits 1000ms after typing "First"
+  strings: [
+    "Developer",
+    "Designer",
+    "Photoshop"
+  ],
+  
+  typeSpeed:60,
+  backSpeed:60,
+  loop:true
+});
+
+
+
+
